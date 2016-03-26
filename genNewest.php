342967e33d0a7ba330829c5b0ddab9703db4d440
Mario Slepcevic <sle3.14@gmail.com>
2016-03-26 15:42:01 +0100

    generated 31 of the newest additions

--- genNewest.php
+++ genNewest.php
@@ -6,7 +6,7 @@
 		include "functions.php";
 		$db = new JHoDisc();
 		$db->connect();
-		if ($db->select("`subrelease` JOIN `release` ON release.id=subrelease.id_release JOIN `names` on names.id=release.id_name","subrelease.name as name,names.name as album,id_release as id,subrelease.id as id2,band,type,format",null,"subrelease.pubDate DESC LIMIT 15")){
+		if ($db->select("`subrelease` JOIN `release` ON release.id=subrelease.id_release JOIN `names` on names.id=release.id_name","subrelease.name as name,names.name as album,id_release as id,subrelease.id as id2,band,type,format",null,"subrelease.pubDate DESC LIMIT 31")){
 			$nameRow = $db->getResult();
 			foreach ($nameRow as $item){
 				echo "<a href='/releases/".$item['band']."/".$item['format']."/".$item['type']."/".$item['id']."/".$item['id2']."#n".$item['id2']."'>".$db->bandShortNames[$item['band']]." - ".$item['album']." - ".$item['name']."</a><br/>";
